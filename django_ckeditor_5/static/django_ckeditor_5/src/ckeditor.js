4b9fb24f8475995cc6e410580194c7bb866b3b2c
Victoria Kaleda <victoria@wrk.com>
2022-06-29 10:21:12 +0000

    Add markdown plugin

--- django_ckeditor_5/static/django_ckeditor_5/src/ckeditor.js
+++ django_ckeditor_5/static/django_ckeditor_5/src/ckeditor.js
@@ -38,6 +38,7 @@ import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
 
 
 export default class ClassicEditor extends ClassicEditorBase {
@@ -76,5 +77,6 @@ ClassicEditor.builtinPlugins = [
     TodoList,
     ListProperties,
     SourceEditing,
-    GeneralHtmlSupport
+    GeneralHtmlSupport,
+    Markdown,
 ];
